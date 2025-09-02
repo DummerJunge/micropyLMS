cc80f7cfc16b4145a216d63736b681b2ceb0276d
goodeb <30559414+goodeb@users.noreply.github.com>
2025-09-02 14:32:52 -0700

    Bug fixes

--- src/micropyLMS/__init__.py
+++ src/micropyLMS/__init__.py
@@ -6,4 +6,6 @@ Music Server (LMS, nee Squezebox Server) systems
 https://lyrion.org/reference/lyrion-music-server/
 """
 
+from .micropyLMS import build_url, core_query, get_players, get_player, Player
+
 __all__ = ['build_url','core_query','get_players','get_player','Player']
